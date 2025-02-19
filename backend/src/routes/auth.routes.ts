b519e53bbbb7950fc78df62885a74c5c491e50b4
Illouminus <professionnel.baillot@gmail.com>
2025-02-19 20:19:14 +0100

    adding google auth script to front and have been starting back handler

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { register, login, verify2FA } from "../controllers/auth.controller";
+import { register, login, verify2FA, googleAuth } from "../controllers/auth.controller";
 
 export default async function authRoutes(fastify: FastifyInstance) {
-  fastify.post("/register", register);
-  fastify.post("/login", login);
-  fastify.post("/verify-2fa", verify2FA);
+	fastify.post("/register", register);
+	fastify.post("/login", login);
+	fastify.post("/verify-2fa", verify2FA);
+	fastify.post("google-authenticator", googleAuth);
 }
