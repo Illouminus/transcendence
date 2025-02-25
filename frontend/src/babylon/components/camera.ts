f93adb2c912de08ec31031b8d82b81341e510cc6
Adrien Hors <adrienhors1@gmail.com>
2025-02-25 18:17:15 +0100

    Function to create players

--- frontend/src/babylon/components/camera.ts
+++ frontend/src/babylon/components/camera.ts
@@ -1,7 +1,7 @@
 import * as BABYLON from 'babylonjs';
 
 export function createCamera(scene: BABYLON.Scene, canvas: HTMLCanvasElement): BABYLON.Camera {
-  const camera = new BABYLON.ArcRotateCamera('camera1', Math.PI / 2, Math.PI / 2, 10, BABYLON.Vector3.Zero(), scene);
-  camera.attachControl(canvas, true);
+  var camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(60), BABYLON.Tools.ToRadians(55), 20, BABYLON.Vector3.Zero(), scene);
+  camera.setTarget(BABYLON.Vector3.Zero());
   return camera;
 }
