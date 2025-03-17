39b044047c2b582c91150df49db354ea4490ee76
Illouminus <professionnel.baillot@gmail.com>
2025-03-17 09:37:13 +0100

    adding enabled and disabled 2FA functions on backend

---
+++ backend/auth/src/controllers/2fa.controller.ts
@@ -0,0 +1,66 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { getErrorMessage, getErrorStatusCode, logError } from "../utils/errorHandler";
+import { getUserById } from "../models/user.model";
+import { disableTwoFactorAuth, enableTwoFactorAuth } from "../models/2fa.models";
+import { issueAndSetToken, verifyTwoFactorAuth } from "../services/auth.service";
+import { TwoFABody } from "../@types/auth.types";
+
+
+
+export async function enable2FAController(req: FastifyRequest, res: FastifyReply) {
+    try {
+        const userIdHeader = req.headers['x-user-id'];
+		if (!userIdHeader) {
+			return res.status(401).send({ error: "User ID not provided" });
+		}
+		const userId = parseInt(userIdHeader as string, 10);
+
+        const user = await getUserById(userId);
+        if (!user) {
+            return res.status(400).send({ error: "User not found" });
+        }
+        if (user.two_factor_enabled) {
+            return res.status(400).send({ error: "Two factor authentication is already enabled" });
+        }
+        await enableTwoFactorAuth(user.id);
+        return res.send({ message: "Two factor authentication enabled" });
+    } catch (error) {
+        return res.status(getErrorStatusCode(error)).send({ error: getErrorMessage(error) });
+    }
+}
+
+
+
+export async function disable2FAController(req: FastifyRequest, res: FastifyReply) {
+    try {
+        const userIdHeader = req.headers['x-user-id'];
+		if (!userIdHeader) {
+			return res.status(401).send({ error: "User ID not provided" });
+		}
+		const userId = parseInt(userIdHeader as string, 10);
+
+        const user = await getUserById(userId);
+        if (!user) {
+            return res.status(400).send({ error: "User not found" });
+        }
+        if (!user.two_factor_enabled) {
+            return res.status(400).send({ error: "Two factor authentication is not enabled" });
+        }
+        await disableTwoFactorAuth(user.id);
+        return res.send({ message: "Two factor authentication disabled" });
+    } catch (error) {
+        return res.status(getErrorStatusCode(error)).send({ error: getErrorMessage(error) });
+    }
+}
+
+
+
+export async function verify2FAController( req: FastifyRequest<{Body: TwoFABody}>, res: FastifyReply,) {
+    try {
+        const response = await verifyTwoFactorAuth( res.server, req.body.email, req.body.code);
+        await issueAndSetToken(res.server, res, response);
+        return res.send({ message: "Login successful!" });
+    } catch (error) {
+        return res.status(400).send({ error: getErrorMessage(error) });
+    }
+}
--- backend/auth/src/controllers/auth.controller.ts
+++ backend/auth/src/controllers/auth.controller.ts
@@ -13,17 +13,11 @@ export async function loginController( req: FastifyRequest<{ Body: LoginBody }>,
 		const response = await loginUser(req.body.email, req.body.password);
 		if(response.user)
 		{
-			const publicProfile: UserPublic = {
-				id: response.user.id,
-				is_verified: response.user.is_verified,
-				two_factor_enabled: response.user.two_factor_enabled,
-				email: response.user.email
-			};
-
 			await issueAndSetToken(res.server, res, response.user.id);
-			return res.status(200).send({message: "Login successful", user: publicProfile });
+			return res.status(200).send( "Login successful");
 		}
-		return res.status(200).send({ message: response.message });
+		else
+		 throw new Error("Login failed");
 	} catch (error) {
 		return res.status(400).send({ error: getErrorMessage(error) });
 	}
@@ -39,6 +33,7 @@ export async function getUserInfoController(req: FastifyRequest, res: FastifyRep
 		}
 		const userId = parseInt(userIdHeader as string, 10);
 		const user = await getUserById(userId);
+
 		if (!user) {
 			return res.status(404).send({ error: "User not found" });
 		}
@@ -50,7 +45,7 @@ export async function getUserInfoController(req: FastifyRequest, res: FastifyRep
 		};
 		return res.status(200).send({ user: publicProfile });
 	} catch (error) {
-		return res.status(400).send({ error: getErrorMessage(error) });
+		return res.status(getErrorStatusCode(error)).send({ error: getErrorMessage(error) });
 	}
 }
 
@@ -66,23 +61,12 @@ export async function verifyEmailController(req: FastifyRequest<{Querystring: {t
 			return res.status(400).send({ error: "Invalid token" });
 		}
 		await verifyEmail(user.id);
+
 		return res.redirect(`${process.env.FRONTEND_URL}/login`);
 	} catch (error) {
 		return res.status(400).send({ error: getErrorMessage(error) });
 	}
 }
-
-
-export async function verify2FAController( req: FastifyRequest<{Body: TwoFABody}>, res: FastifyReply,) {
-	try {
-		const response = await verifyTwoFactorAuth( res.server, req.body.email, req.body.code);
-		await issueAndSetToken(res.server, res, response);
-		return res.send({ message: "Login successful!" });
-	} catch (error) {
-		return res.status(400).send({ error: getErrorMessage(error) });
-	}
-}
-
 
 export async function googleAuthLogin( req: FastifyRequest<{ Body: { idToken: string } }>, res: FastifyReply) {
 	try {
@@ -95,19 +79,11 @@ export async function googleAuthLogin( req: FastifyRequest<{ Body: { idToken: st
 		if (!user) {
 			return res.status(400).send({ error: "Login failed" });
 		}
-
-
-		const pubclicProfile: UserPublic = {
-			id: user.id,
-			is_verified: user.is_verified,
-			two_factor_enabled: user.two_factor_enabled,
-			email: user.email,
-		};
 		await issueAndSetToken(res.server, res, user.id);
-		return res.status(200).send({ message: "Login successful!", user: pubclicProfile });
+		return res.status(200).send("Login successful!");
 		
 	} catch (error) {
-		return res.status(400).send({ error: getErrorMessage(error) });
+		return res.status(getErrorStatusCode(error)).send({ error: getErrorMessage(error) });
 	}
 }
 
@@ -121,7 +97,6 @@ export async function registerController(req: FastifyRequest<{Body: RegisterUser
 
 	  const user = await registerUserService( username, email, password);
 	  publishToQueue("user.registered", { userId: user.id, email: user.email, username: user.username });
-	  
 	  return reply.status(201).send({message : "User registered!"});
 	} catch (error) {
 	  logError(error, "registerUser");
@@ -138,6 +113,8 @@ export async function logoutController(req: FastifyRequest, res: FastifyReply) {
 		res.status(401).send({ error: error.message });
 	}
 }
+
+
 
 export async function updateController(req: FastifyRequest<{Body: RegisterUser}>, res: FastifyReply) {
 	try {
@@ -160,10 +137,11 @@ export async function updateController(req: FastifyRequest<{Body: RegisterUser}>
 		else 
 		{
 			publishToQueue("user.updated", { userId: user.id, username: user.username, email: user.email });
-			
 			res.status(200).send({ message: "User updated successfully" });
 		}
 	} catch (error: any) {
 		res.status(401).send({ error: error.message });
 	}
 }
+
+
